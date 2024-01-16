2c2d02aa0fb34585cbd672e436e87f5a344d5ed5
itspayalsingh <ps56550856@gmail.com>
2024-01-16 12:46:28 +0530

    intialized prt

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express=require("express")
 const app= express()
 
 app.get("/",(req,res)=>{
-    console.log(req.ip);
-    res.send("nth")
+    console.log("ip",req.ip);
+    res.send("nth",req.ip)
 })
 
 
