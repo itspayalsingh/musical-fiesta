e32e19a3795617704997c1806417fd115370b994
itspayalsingh <ps56550856@gmail.com>
2024-01-18 12:42:11 +0530

    intialized t

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const express=require("express")
 const app= express()
 var ip = require('ip');
+const dns= require("dns")
 // var ipLocation = require('ip-location')
 app.use(express.json()) 
  
 
- 
+//  dns.lookup("https://stormy-tan-fossa.cyclic.app/",(err,ans)=>{
+//     console.log(ans);
+//  })
 
-let add=ip.address()
-console.log(add);
-app.get("/",(req,res)=>{
-    // console.log("ip",req.ip);
-    res.send(add)
-})
+//  const dns = require('dns');
+//  const url = require('url');
+ 
+//  let hostname = url.parse("https://stormy-tan-fossa.cyclic.app/").hostname;
+ 
+// let hostname = url.parse("https://www.cyclic.sh/").hostname;
+//  dns.lookup(hostname, (err, address, family) => {
+//    console.log('address: %j family: IPv%s', address, family);
+//  });
+ 
+app.get('/', (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log('IP Address of the client is: ', ip);
+  res.send('Hello World!');
+});
+
+
+
+
+
+// let add=ip.address()
+// // console.log(add);
+// app.get("/",(req,res)=>{
+
+//     // console.log("ip",req.ip);
+//     res.send(add)
+// })
 
 
 app.listen(5050,()=>{
