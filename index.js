e385a0904163b5ba34eab02360675446ed4be0cc
itspayalsingh <ps56550856@gmail.com>
2024-01-18 13:22:22 +0530

    intialized t

--- index.js
+++ index.js
@@ -3,29 +3,20 @@ const app= express()
 var ip = require('ip');
 const dns= require("dns")
 const fs=require("fs")
+var geoip = require('geoip-lite');
 // var ipLocation = require('ip-location')
 app.use(express.json()) 
  
-
-//  dns.lookup("https://stormy-tan-fossa.cyclic.app/",(err,ans)=>{
-//     console.log(ans);
-//  })
-
-//  const dns = require('dns');
-//  const url = require('url');
  
-//  let hostname = url.parse("https://stormy-tan-fossa.cyclic.app/").hostname;
  
-// let hostname = url.parse("https://www.cyclic.sh/").hostname;
-//  dns.lookup(hostname, (err, address, family) => {
-//    console.log('address: %j family: IPv%s', address, family);
-//  });
  
 app.get('/', (req, res) => {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  console.log('IP Address of the client is: ', ip);
+  
+  var geo = geoip.lookup(ip);
+  console.log('IP Address of the client is: ', geo);
   // fs.appendFileSync("./ans.txt",ip)
-  res.send(ip);
+  res.send(geo);
 });
 
 
